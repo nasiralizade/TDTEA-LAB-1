470cd9663f794379ca037999b20ad5d4ca71ad48
nasiralizade <nasir.alizade1@gmail.com>
2024-02-08 00:59:30 +0100

    merged v5 to main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "Parse.h"
 
-
 int main() {
     std::string input = "(hate+love)";
     std::string text = "Waterloo I was defeated, you won the war Waterloo promise to love you for ever more Waterloo couldn't escape if I wanted to Waterloo knowing my fate is to be with you Waterloo finally facing my Waterloo";
