a4ac43af27949085dc99ff433708f2932f636aad
nasiralizade <nasir.alizade1@gmail.com>
2024-02-08 06:06:56 +0100

    deleted some file

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "Parse.h"
 
 int main() {
-    std::string input = "(hate+love)";
+    std::string input = "lo* could.{3}";
     std::string text = "Waterloo I was defeated, you won the war Waterloo promise to love you for ever more Waterloo couldn't escape if I wanted to Waterloo knowing my fate is to be with you Waterloo finally facing my Waterloo";
     auto first = input.begin();
     auto last = input.end();
