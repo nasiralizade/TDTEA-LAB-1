9480dcbae6d509171adede953be18820351e0c5a
nasiralizade <nasir.alizade1@gmail.com>
2024-02-13 00:41:07 +0100

    denna version har allt fungerande

--- Parse.cpp
+++ Parse.cpp
@@ -9,13 +9,17 @@
  */
 op *Parse::parse_expr() {
   auto lhs = parse_subexpression();
+  auto sub_expr = new Subexpression();
+  sub_expr->add(lhs);
   while (lexer.get_current().type == OR_OP) {
-        lexer.advance();
+    lexer.advance();
     auto rhs = parse_subexpression();
     auto or_op_p = new or_op();
-    or_op_p->add(lhs);
+    or_op_p->add(sub_expr->children.back());
+    sub_expr->children.pop_back();
     or_op_p->add(rhs);
-    lhs = or_op_p;
+    sub_expr->add(or_op_p);
+    lhs = sub_expr;
   }
   return lhs;
 }
@@ -35,7 +39,9 @@ op *Parse::parse_subexpression() {
       sub_expr = dynamic_cast<Subexpression *>(operation);
     } else {
       auto element = parse_element();
-      sub_expr->add(element);
+      if (element) {
+        sub_expr->add(element);
+      }
     }
   }
   if (sub_expr->children.size() == 1) {
@@ -48,23 +54,18 @@ op *Parse::parse_subexpression() {
 }
 
 /**
- * element   ::= component | exact_op
+ * element   ::= component | component'{number}'
  * @return
  */
 op *Parse::parse_element() {
-    auto component = parse_component();
-    if (lexer.get_current().type == L_BRACKET) {
-        lexer.advance();
-      int number = get_number();
-      if (lexer.get_current().type != R_BRACKET) {
-            std::cerr << "missing }\n";
-        }
-        lexer.advance();
-        auto count_op = new exact_op(number);
-        count_op->add(component);
-        return count_op;
-    }
-    return component;
+  auto component = parse_component();
+  if (lexer.get_current().type == L_BRACKET) {
+    int number = get_number();
+    auto count_op = new exact_op(number);
+    count_op->add(component);
+    return count_op;
+  }
+  return component;
 }
 
 /**
@@ -79,26 +80,24 @@ op *Parse::parse_operation(op *lhs) {
     repeat_op_p->add(lhs->children.back());
     lhs->children.pop_back();
     lhs->add(repeat_op_p);
+    return lhs;
   } else if (lexer.get_current().type == SLASH) {
-        lexer.advance();
-        char next = lexer.get_current().text[0];
-        if (next == 'I') {
-            lexer.advance();
-          auto ignore_op_p = new ignore_case_op(lhs->children.back());
-
-          ignore_op_p->set_ignore_case(true);
-          lhs->children.pop_back();
-          lhs->add(ignore_op_p);
-        } else if (next == 'O') {
-          lexer.advance();
-          lexer.advance(); // skip the next {
-          int groupNum = get_number();
-          lexer.advance(); // skip the next }
-          auto output_group_op_p = new output_group_op(groupNum);
-          output_group_op_p->add(lhs);
-          lhs->add(output_group_op_p);
-        }
-        return lhs;
+    lexer.advance();
+    char next = lexer.get_current().text[0];
+    if (next == 'I') { // ignore case '\I'
+      lexer.advance();
+      auto ignore_op_p = new ignore_case_op(lhs->children.back());
+      ignore_op_p->set_ignore_case(true);
+      lhs->children.pop_back();
+      lhs->add(ignore_op_p);
+    } else if (next == 'O') { // output group '\O{number}'
+      lexer.advance();
+      int groupNum = get_number();
+      auto output_group_op_p = new output_group_op(groupNum);
+      output_group_op_p->add(lhs);
+      lhs->add(output_group_op_p);
+    }
+    return lhs;
   }
   return nullptr;
 }
@@ -136,31 +135,40 @@ bool Parse::is_valid_char(char c) {
 }
 
 int Parse::get_number() {
+  if (lexer.get_current().type != L_BRACKET) {
+    std::cerr << "missing {\n";
+    return 0;
+  }
+  lexer.advance();
   std::string number;
   while (lexer.get_current().type == LETTER) {
     number += lexer.get_current().text;
     lexer.advance();
   }
+  if (lexer.get_current().type != R_BRACKET) {
+    std::cerr << "missing }\n";
+  }
+  lexer.advance();
   return std::stoi(number);
 }
 
 bool Parse::match_from_any_pos(std::string &input) {
-    char *first = &input[0];
-    char *last = &input[input.size()];
-    auto pattern = parse_expr();
-    while (first != last) {
-        char *temp = first;
-        if (pattern->eval(first, last)) {
-            matchedSubstring = std::string(temp, first);
-            if (group_index_ > 0) {
-              std::cout << capturedGroups[group_index_ - 1] << std::endl;
-              return true;
-            }
-            std::cout << "Matched: " << matchedSubstring
-                      << std::endl; // Skriv ut den matchade delsträngen
-            return true;
-        }
-        first = temp + 1;
+  char *first = &input[0];
+  char *last = &input[input.size()];
+  auto pattern = parse_expr();
+
+  while (first != last) {
+    char *temp = first;
+    if (pattern->eval(first, last)) {
+      matchedSubstring = std::string(temp, first);
+      if (group_index_ > 0) {
+        std::cout << capturedGroups[group_index_ - 1] << std::endl;
+        return true;
+      }
+      std::cout << "Matched: " << matchedSubstring << std::endl;
+      return true;
     }
-    return false;
+    first = temp + 1;
+  }
+  return false;
 }
